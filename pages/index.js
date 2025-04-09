a51c7d798f76fc9b0e46bb10a2af6f27a653ec2b
Rameezhh <raheview@gmail.com>
2025-04-09 08:47:58 -0700

    Updated

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,7 @@ export default function Home() {
         <div className="mt-20 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
           {/* <h1 className="text-2xl text-bold">Work.</h1> */}
           <h1 className="my-8 mb-0 tablet:m-8 ta mt-[30px] text-3xl uppercase poppins-bold  opacity-80">
-            Projects.
+            Projects
           </h1>
 
           {/* <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-2"> */}
